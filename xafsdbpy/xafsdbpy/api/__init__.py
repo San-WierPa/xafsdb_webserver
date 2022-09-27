3299f05adc073f34a3c67e7d66acd5c6db181b27
Sebastian Paripsa <sebastian.wiercinski2011@gmail.com>
2022-09-27 17:34:35 +0200

    WIP heroku

--- xafsdbpy/xafsdbpy/api/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from xafsdbpy.api.dataset_api import DatasetApi
-from xafsdbpy.api.file_api import FileApi
-from xafsdbpy.api.item_api import ItemApi
-from xafsdbpy.api.user_api import UserApi
