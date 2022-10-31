9a24376920deca35308611c3ba4a8a303b098f38
Sebastian Paripsa <sebastian.wiercinski2011@gmail.com>
2022-10-31 22:07:37 +0100

    formatting

--- webserver/settings.py
+++ webserver/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-#import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,4 +154,4 @@ STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-#django_heroku.settings(locals())
+# django_heroku.settings(locals())
