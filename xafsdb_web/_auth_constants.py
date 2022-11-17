420288f14df22105d7001175c4f9b84601a9c36f
Sebastian Paripsa <sebastian.wiercinski2011@gmail.com>
2022-11-17 20:04:15 +0100

    WIP docker

--- xafsdb_web/_auth_constants.py
+++ xafsdb_web/_auth_constants.py
@@ -1,8 +1,15 @@
 import scicat_py
 
+#USERNAME = "admin"
+#PASSWORD = "2jf70TPNZsS"
+#
+#CONFIGURATION = scicat_py.Configuration(
+#    host="http://35.205.92.39",
+#)
+
 USERNAME = "admin"
-PASSWORD = "2jf70TPNZsS"
+PASSWORD = "2jf70TPNZsS_xafs"
 
 CONFIGURATION = scicat_py.Configuration(
-    host="http://35.205.92.39",
+    host="http://scicat:3000",
 )
