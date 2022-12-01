c4d162a6255baca7956bfb705e593ce573aee0ca
Sebastian Paripsa <sebastian.wiercinski2011@gmail.com>
2022-12-01 16:03:10 +0100

    format + matplotlib-backend error fix

--- quality control/Criteria_json.py
+++ quality control/Criteria_json.py
@@ -8,61 +8,90 @@ Created on Thu Jan 28 15:19:22 2021
 import json
 import os
 
+criteria = {
+    "SYNCHROTRON": {
+        "ABSORPTION": {
+            "RAW": {
+                "edge step": {
+                    "min": 0.5,  # alias: edge jump
+                    "max": 2.0,
+                    "unit": "1",
+                    "documentation": "height of the detected edge step",
+                },
+                "k max": {
+                    "min": 15.0,
+                    "max": 20.0,
+                    "unit": "1/angstrom",
+                    "documentation": "considered angular wavenumber",
+                },
+                "noise": {
+                    "min": 1,  ### not that clear, depends on method, Abhijeet will "figure it out"
+                    "max": 5,  ### further discussion needed --> research methods define criteria (example SNR)
+                    "unit": "1",
+                    "documentation": "noise of the measurement",
+                },
+                "energy resolution": {
+                    "min": 0.2,
+                    "max": 2.0,
+                    "unit": "eV",
+                    "documentation": "energy resolution of the measured spectrum in eV",
+                },
+            },
+            "PROCESSED": {
+                "amplitude reduction factor": {
+                    "Z-ranges": {
+                        "[10,40[": {"min": 0.7, "max": 1.0},
+                        "[40,80[": {"min": 0.7, "max": 1.0},
+                    },
+                    "unit": "1",
+                    "documentation": "amplitude factor from the processed spectrum",
+                },
+            },
+        },
+    },
+    ### Laboratory values are only blind values. No decision up to now
+    "LABORATORY": {
+        "ABSORPTION": {
+            "RAW": {
+                "edge step": {
+                    "min": 0.5,  # alias: edge jump
+                    "max": 2.0,
+                    "unit": "1",
+                    "documentation": "height of the detected edge step",
+                },
+                "k max": {
+                    "min": 10.0,
+                    "max": 20.0,
+                    "unit": "1/angstrom",
+                    "documentation": "considered angular wavenumber",
+                },
+                "noise": {
+                    "min": 1,  ### not that clear, depends on method, Abhijeet will "figure it out"
+                    "max": 5,  ### further discussion needed --> research methods define criteria (example SNR)
+                    "unit": "1",
+                    "documentation": "noise of the measurement",
+                },
+                "energy resolution": {
+                    "min": 0.2,
+                    "max": 2.0,
+                    "unit": "eV",
+                    "documentation": "energy resolution of the measured spectrum in eV",
+                },
+            },
+            "PROCESSED": {
+                "amplitude reduction factor": {
+                    "Z-ranges": {
+                        "[10,40[": {"min": 0.7, "max": 1.0},
+                        "[40,80[": {"min": 0.7, "max": 1.0},
+                    },
+                    "unit": "1",
+                    "documentation": "amplitude factor from the processed spectrum",
+                },
+            },
+        },
+    },
+}
 
-criteria = {'SYNCHROTRON': {'ABSORPTION' : {'RAW' : {'edge step' : {'min' : 0.5,  # alias: edge jump 
-                                                                            'max' : 2.,
-                                                                            'unit' : '1',
-                                                                            'documentation' : 'height of the detected edge step'},
-                                                              'k max' : {'min' : 15.,
-                                                                         'max' : 20.,
-                                                                         'unit' : '1/angstrom',
-                                                                         'documentation' : 'considered angular wavenumber'},
-                                                              'noise' : {'min' : 1, ### not that clear, depends on method, Abhijeet will "figure it out"
-                                                                         'max' : 5, ### further discussion needed --> research methods define criteria (example SNR)
-                                                                         'unit' : '1',
-                                                                         'documentation' : 'noise of the measurement'},
-                                                              'energy resolution' : {'min' : 0.2,
-                                                                                     'max' : 2.,
-                                                                                     'unit' : 'eV',
-                                                                                     'documentation' : 'energy resolution of the measured spectrum in eV'},},
-                                           'PROCESSED' : {'amplitude reduction factor' : {'Z-ranges' : {'[10,40[' : {'min' : 0.7,
-                                                                                                                     'max' : 1.},
-                                                                                                        '[40,80[' : {'min' : 0.7, 
-                                                                                                                     'max' : 1.},},
-                                                                                          'unit' : '1',
-                                                                                          'documentation' : 'amplitude factor from the processed spectrum'},
-                                                          },
-                                            },
-                            },
-            ### Laboratory values are only blind values. No decision up to now
-            'LABORATORY': {'ABSORPTION' : {'RAW' : {'edge step' : {'min' : 0.5,  # alias: edge jump 
-                                                                                        'max' : 2.,
-                                                                                        'unit' : '1',
-                                                                                        'documentation' : 'height of the detected edge step'},
-                                                             'k max' : {'min' : 10.,
-                                                                        'max' : 20.,
-                                                                        'unit' : '1/angstrom',
-                                                                        'documentation' : 'considered angular wavenumber'},
-                                                             'noise' : {'min' : 1, ### not that clear, depends on method, Abhijeet will "figure it out"
-                                                                        'max' : 5, ### further discussion needed --> research methods define criteria (example SNR)
-                                                                        'unit' : '1',
-                                                                        'documentation' : 'noise of the measurement'},
-                                                             'energy resolution' : {'min' : 0.2,
-                                                                                    'max' : 2.,
-                                                                                    'unit' : 'eV',
-                                                                                    'documentation' : 'energy resolution of the measured spectrum in eV'},},
-                                           'PROCESSED' : {'amplitude reduction factor' : {'Z-ranges' : {'[10,40[' : {'min' : 0.7,
-                                                                                                                     'max' : 1.},
-                                                                                                        '[40,80[' : {'min' : 0.7, 
-                                                                                                                     'max' : 1.},},
-                                                                                          'unit' : '1',
-                                                                                          'documentation' : 'amplitude factor from the processed spectrum'},
-                                                          },
-                                           },
-                           },
-            }
-
-criteria_path = os.path.abspath(os.path.curdir)+'/Criteria.json'
-with open(criteria_path, 'w') as tofile:
-    json.dump(criteria, fp = tofile, indent = 4,
-              ensure_ascii = False)
+criteria_path = os.path.abspath(os.path.curdir) + "/Criteria.json"
+with open(criteria_path, "w") as tofile:
+    json.dump(criteria, fp=tofile, indent=4, ensure_ascii=False)
