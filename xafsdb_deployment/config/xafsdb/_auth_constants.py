a27ad0723b92d586d99e38ee9291da66d25a599e
Sebastian Paripsa <sebastian.wiercinski2011@gmail.com>
2023-01-11 16:00:13 +0100

    WIP compose security fix service:xafsdb

--- xafsdb_deployment/config/xafsdb/_auth_constants.py
+++ xafsdb_deployment/config/xafsdb/_auth_constants.py
@@ -3,9 +3,13 @@
 """
 
 import scicat_py
+import environ
 
-USERNAME = "admin"
-PASSWORD = "1234"
+env = environ.Env()
+environ.Env.read_env()
+
+USERNAME = env("USERNAME")
+PASSWORD = env("PASSWORD")
 
 CONFIGURATION = scicat_py.Configuration(
     host="http://scicat:3000",
