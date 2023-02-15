4347b6a8d6d0b4396c1d2d53054ca5aaa7d214f0
Sebastian Paripsa <sebastian.wiercinski2011@gmail.com>
2023-02-15 21:44:52 +0100

    WIP server not running

--- webserver/settings.py
+++ webserver/settings.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+
+"""
+@author: Sebastian Paripsa
+"""
+
 import os
 from pathlib import Path
 
@@ -30,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # while giving remote access to the localhost via "pagekite.py 8001 xafsdb.pagekite.me"
 # uncomment following and change "test/" in urls.py to "" and "" to "test/":
