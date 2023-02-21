a4e9642d80996525f7d51e549539b08eb0498d54
Sebastian Paripsa <sebastian.wiercinski2011@gmail.com>
2023-02-21 22:22:15 +0100

    scicat api client

---
+++ scicat_py/scicat_py/api/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from scicat_py.api.auth_api import AuthApi
+from scicat_py.api.datasets_api import DatasetsApi
+from scicat_py.api.instruments_api import InstrumentsApi
+from scicat_py.api.jobs_api import JobsApi
+from scicat_py.api.logbooks_api import LogbooksApi
+from scicat_py.api.policies_api import PoliciesApi
+from scicat_py.api.proposals_api import ProposalsApi
+from scicat_py.api.published_data_api import PublishedDataApi
+from scicat_py.api.samples_api import SamplesApi
+from scicat_py.api.user_identities_api import UserIdentitiesApi
+from scicat_py.api.users_api import UsersApi
+
+# flake8: noqa
